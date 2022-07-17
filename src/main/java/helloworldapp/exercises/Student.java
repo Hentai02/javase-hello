26a2194e239a77ea82670f2bc72f4244679608ec
hentai02 <hentaikis@gmail.com>
2022-07-17 21:38:14 +0800

    202207172138

---
+++ src/main/java/helloworldapp/exercises/Student.java
@@ -0,0 +1,58 @@
+package helloworldapp.exercises;
+
+public class Student implements StudentAction{
+    private int id;
+    private String name;
+    private boolean gender;
+    private String cellphone;
+
+    public Student() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isGender() {
+        return gender;
+    }
+
+    public void setGender(boolean gender) {
+        this.gender = gender;
+    }
+
+    public String getCellphone() {
+        return cellphone;
+    }
+
+    public void setCellphone(String cellphone) {
+        this.cellphone = cellphone;
+    }
+
+    @Override
+    public void studying() {
+
+    }
+
+    @Override
+    public void goSchool() {
+
+    }
+
+    @Override
+    public void afterSchool() {
+
+    }
+}
---
+++ src/main/java/helloworldapp/exercises/StudentAction.java
@@ -0,0 +1,9 @@
+package helloworldapp.exercises;
+
+public interface StudentAction {
+    void studying ();
+
+    void goSchool();
+
+    void afterSchool();
+}
